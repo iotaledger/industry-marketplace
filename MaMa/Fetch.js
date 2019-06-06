66818ec7dfcde71d46f437fe52749158085883eb
LisaSchneider94 <lisa.schneider@iota.org>
2019-06-06 22:53:28 +0200

    update async

--- MaMa/Fetch.js
+++ MaMa/Fetch.js
@@ -1,20 +1,52 @@
 const Iota = require('@iota/core');
 const Converter = require('@iota/converter');
+let zmq = require('zeromq');
+let sock = zmq.socket('sub');
+var WebSocket = require('ws');
+
+//var ws = new WebSocket("wss://echo.websocket.org");
+var ws = new WebSocket("ws://127.0.0.1:1880/ws/sp")
 
 const iota = Iota.composeAPI({
         provider: 'https://nodes.devnet.iota.org:443'
         });
     
+exports.fetchFromZMQ = (tag) => {
 
+        sock.connect('tcp://zmq.devnet.iota.org:5556');
+        //Subscribe to the confirmed transactions event
+        sock.subscribe('tx');
+        
+        
+        //Create a callback function to process the data that is returned from the ZMQ
+        sock.on('message', msg => {
+        //Split the data into an array
+        
+        const data = msg.toString().split(' ');
+        
+        //Filter for certain tag
+        if (data[12] == tag)
+        {
+        
+                 const get = async () => {
+                      let message = await fetchFromTangle(data)
+                      console.log(message) // publish.sendToUI(message, role)
+                 }
+                
+                get();
+                
+        }
+          
+        });
+         
+}
+        
 
-exports.fetchFromTangle = (data) => 
+fetchFromTangle = (data) => 
 
 { 
-
  return new Promise(resolve => {
                
-          
-       
          //Get bundle and extract data
 var bundle = data[8]  
 
