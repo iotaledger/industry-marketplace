6ba0016feface8809d854390401edba40089fb52
Alexey Sobolev <sobolev@mail.de>
2019-06-11 10:42:08 +0200

    Refactored wallet address generator

--- MaMa/wallet.js
+++ MaMa/wallet.js
@@ -1,30 +1,27 @@
-// Require the IOTA libraries
-const Iota = require('@iota/core');
-const Converter = require('@iota/converter');
+const { composeAPI } = require('@iota/core');
+const crypto = require('crypto');
+
+const generateSeed = (length = 81) => {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+    let seed = '';
+    while (seed.length < length) {
+        const byte = crypto.randomBytes(1)
+        if (byte[0] < 243) {
+            seed += charset.charAt(byte[0] % 27);
+          }
+    }
+    return seed;
+};
+
 // Create a new instance of the IOTA object
 // Use the `provider` field to specify which IRI node to connect to
-const iota = Iota.composeAPI({
-provider: 'https://nodes.devnet.iota.org:443'
-});
+const provider = 'https://nodes.devnet.iota.org:443';
+const iota = composeAPI({ provider });
 
-//Create seed from terminal
-//Linux: cat /dev/urandom |tr -dc A-Z9|head -c${1:-81}
-//Let this run through once and catch first Address
-// insert public adress into https://faucet.devnet.iota.org/ to get iota tokens
+const seed = generateSeed();
 
-const seed = "GXJ9NZBEYLNBIKIURJFCSYOATERR9TFCZTHHRICRESIHPSLRIZAZUGNHTCVDZSLO9ZSKTITCRJRWWZTLB"
+iota.getNewAddress(seed)
+  .then(address => console.log(`Address: ${address} \nSeed: ${seed}`))
+  .catch(err => console.log(err));
 
-    iota.getNewAddress(seed, {'returnAll':true})
-        .then(allAddresses => {
-                console.log(allAddresses)
-                return  iota.getBalances(allAddresses, 10)
-        })
-        .then( balances => {
-                console.log(balances)
-        })
-  .catch(err => {
-           
-        // Catch any errors
-            console.log(err);
-        });
-        
+// Insert public adress into https://data.iota.org/#/faucet to get IOTA tokens
