e14819e6c2754c0585c23340ab253c5dbf95aa8e
Alexey Sobolev <sobolev@mail.de>
2019-06-11 11:00:26 +0200

    Refactored transaction script

--- MaMa/transaction.js
+++ MaMa/transaction.js
@@ -1,52 +1,40 @@
 // Require the IOTA libraries
-const Iota = require('@iota/core');
-const Converter = require('@iota/converter');
+const { composeAPI } = require('@iota/core');
+const { asciiToTrytes } = require('@iota/converter');
+const { generateSeed } = require('./helpers');
+
 // Create a new instance of the IOTA object
 // Use the `provider` field to specify which IRI node to connect to
-const iota = Iota.composeAPI({
-provider: 'https://nodes.devnet.iota.org:443'
-});
+const provider = 'https://nodes.devnet.iota.org:443';
+const iota = composeAPI({ provider });
 
+function sendMessage(purpose, dataelements, conversation_ID, message_ID, reply_by, value) {
+    const payload = {
+      'Purpose': purpose,
+      'Dataelements': dataelements,
+      'Conversation_ID': conversation_ID,
+      'Message_ID': message_ID,
+      'Reply_by': reply_by
+    }
 
+    const seed = generateSeed();
 
+    const message = asciiToTrytes(JSON.stringify(payload));
 
-sendMessage("cfp", "Bohren!!!", 766, "836482f", 12022020, 0)
+    const address = 'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD';
 
+    const tag = 'MBPCHDRCWCWBTCICWB9CFA99999';
 
-function sendMessage(purpose, dataelements, conversation_ID, message_ID, reply_by, value)
-{
- var structure = {"Purpose":purpose, "Dataelements":dataelements, "Conversation_ID":conversation_ID, "Message_ID":message_ID, "Reply_by": reply_by}
-
-
-    const seed = "GXJ9NZBEYLNBIKIURJFCSYOATERR9TFCZTHHRICRESIHPSLRIZAZUGNHTCVDZSLO9ZSKTITCRJRWWZTLB"
-
-    const message = Converter.asciiToTrytes(JSON.stringify(structure))
-    //const message = Converter.asciiToTrytes("TAKE MY TOKENS")
-
-    address = "HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD"
-    tag = "MBPCHDRCWCWBTCICWB9CFA99999"
-    //transfer array specifies transfers you want to make 
-    const transfers = [
-        {
-            value: value,
-            address: address,
-            message: message,
-            tag: tag
-        }
-        ];
-        
+    //transfer array specifies transfers you want to make
+    const transfers = [{ value, address, message, tag }];
 
     iota.prepareTransfers(seed, transfers)
-        .then(trytes => {
-            return iota.sendTrytes(trytes, 3, 9)
-        })
-        .then(bundle => {
-    console.log(`Published transaction with tail hash: ${bundle[0].hash}`)
-        console.log(`Bundle: ${JSON.stringify(bundle, null, 1)}`)
-    })
-
-    .catch(err => {
-            // Catch any errors
-        console.log(err);
-    });
+      .then(trytes => iota.sendTrytes(trytes, 3, 9))
+      .then(bundle => {
+          console.log(`Published transaction with tail hash: ${bundle[0].hash}`);
+          console.log(`Bundle: ${JSON.stringify(bundle, null, 1)}`);
+      })
+      .catch(err => console.log(err));
 }
+
+sendMessage('cfp', 'Bohren!!!', 766, '836482f', 12022020, 0);
