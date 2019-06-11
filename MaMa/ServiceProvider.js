02a3a16148bb312e627bb201497f4244c0ce39e8
Alexey Sobolev <sobolev@mail.de>
2019-06-11 13:18:13 +0200

    Prettified code

--- MaMa/ServiceProvider.js
+++ MaMa/ServiceProvider.js
@@ -1,4 +1,4 @@
-// Import libraries and create a ZMQ subscribe socket 
+// Import libraries and create a ZMQ subscribe socket
 let zmq = require('zeromq');
 //Create a <query text="zmq" /> socket
 let sock = zmq.socket('sub');
@@ -7,47 +7,42 @@ var WebSocket = require('ws');
 const Iota = require('@iota/core');
 const Converter = require('@iota/converter');
 
+let functions = require('./Functions.js');
 
-let functions = require('./Functions.js')
+var ws = new WebSocket('ws://127.0.0.1:1880/ws/sp');
 
-var ws = new WebSocket("ws://127.0.0.1:1880/ws/sp")
-
-const role = "sp"
+const role = 'sp';
 const iota = Iota.composeAPI({
     provider: 'https://nodes.devnet.iota.org:443'
-    });
+});
 
- ws.onopen = () => {
-      console.log("CONNECTED")
+ws.onopen = () => {
+    console.log('CONNECTED');
 };
 let tag;
 
- ws.onmessage = (serviceID) => {
-    console.log("Received Service ID" + serviceID + "from UI")
-    serviceID = serviceID.data
-    var abbrServiceID = serviceID.replace(/#/g,'').replace(/-/g,''); 
-    tag = abbrServiceID.substr(0, abbrServiceID.length-3)
-    tag = Converter.asciiToTrytes(tag) + '9'
+ws.onmessage = serviceID => {
+    console.log('Received Service ID' + serviceID + 'from UI');
+    serviceID = serviceID.data;
+    var abbrServiceID = serviceID.replace(/#/g, '').replace(/-/g, '');
+    tag = abbrServiceID.substr(0, abbrServiceID.length - 3);
+    tag = Converter.asciiToTrytes(tag) + '9';
 
     sock.connect('tcp://zmq.devnet.iota.org:5556');
     sock.subscribe('tx');
     sock.on('message', msg => {
-    const data = msg.toString().split(' ');
-    
-    //Filter for certain tag
-    if (data[12] == tag)
-    {
-            console.log("RECEIVED ANOTHER MSG FROM ZMQ")
-             const get = async () => {
-                  let message = await functions.fetchFromTangle(data)
-                  functions.sendToUI(message, role)
-                  console.log("GOT MSG FROM TANGLE")
-            }
-            
-             get();
-            
-    }
-})
-      
-}
- 
+        const data = msg.toString().split(' ');
+
+        //Filter for certain tag
+        if (data[12] == tag) {
+            console.log('RECEIVED ANOTHER MSG FROM ZMQ');
+            const get = async () => {
+                let message = await functions.fetchFromTangle(data);
+                functions.sendToUI(message, role);
+                console.log('GOT MSG FROM TANGLE');
+            };
+
+            get();
+        }
+    });
+};
--- MaMa/ServiceRequest.js
+++ MaMa/ServiceRequest.js
@@ -2,35 +2,28 @@
 const Iota = require('@iota/core');
 const Converter = require('@iota/converter');
 var WebSocket = require('ws');
-// Import libraries and create a ZMQ subscribe socket 
+// Import libraries and create a ZMQ subscribe socket
 let zmq = require('zeromq');
 //Create a <query text="zmq" /> socket
 let sock = zmq.socket('sub');
 var WebSocket = require('ws');
-let functions = require('./Functions.js')
-    
+let functions = require('./Functions.js');
 
 // Create a new instance of the IOTA object
 // Use the `provider` field to specify which IRI node to connect to
 const iota = Iota.composeAPI({
-provider: 'https://nodes.devnet.iota.org:443'
+    provider: 'https://nodes.devnet.iota.org:443'
 });
 
 //var ws = new WebSocket("ws://10.197.0.135:1880/ws/cfp")
-var ws = new WebSocket("ws://127.0.0.1:1880/ws/sr")
+var ws = new WebSocket('ws://127.0.0.1:1880/ws/sr');
 
 ws.onopen = () => {
-    console.log("CONNECTED")
-      };
+    console.log('CONNECTED');
+};
 
-const role = "sr"
-// Get json from websocket 
-ws.onmessage = (msg) => {
-   
-
-   functions.sendMessage(msg.data)
-   
- }
-
-
-
+const role = 'sr';
+// Get json from websocket
+ws.onmessage = msg => {
+    functions.sendMessage(msg.data);
+};
