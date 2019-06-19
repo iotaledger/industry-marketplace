8b212dc2de048813ae3a8ba6ae90babde8db09af
Alexey Sobolev <sobolev@mail.de>
2019-06-19 15:33:37 +0200

    Utility functions to check balance and store wallet data in local DB

---
+++ MaMa/db.js
@@ -0,0 +1,42 @@
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+
+const db = levelup(leveldown('../wallet'))
+
+const writeData = async data => {
+    return new Promise(async (resolve, reject) => {
+        db.put('wallet', JSON.stringify(data), function (err) {
+            if (err) reject(err) // some kind of I/O error
+            resolve();
+        })
+    });
+}
+
+const readData = async () => {
+    return new Promise(async (resolve, reject) => {
+        db.get('wallet', function (err, value) {
+            if (err) reject(err) // likely the key was not found
+            const result = JSON.parse(value.toString());
+            resolve(result)
+        })
+    });
+}
+
+module.exports = {
+    readData,
+    writeData,
+}
+
+// const data = {
+//     address: 'CCCCCDDDDD',
+//     seed: 'SSSSSSEEEEDDDD',
+//     amount: 555
+// }
+
+// const run = async () => {
+//     await writeData(data)
+//     const result = await readData()
+//     console.log(result.address, result.amount, result.seed)
+// }
+
+// run()
