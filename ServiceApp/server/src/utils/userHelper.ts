04fbbff34c85f844d63ce122061f1a7324dc7c83
Alexey Sobolev <sobolev@mail.de>
2019-06-20 15:02:19 +0200

    Added user helpers to create new user and wallet

---
+++ ServiceApp/server/src/utils/userHelper.ts
@@ -0,0 +1,33 @@
+import yargs from 'yargs';
+import { writeData } from './db';
+
+const argv = yargs
+	.usage('Create new user or wallet')
+	.example('$0 --create user --role SR --id user-123', 'Creates a new Service Requeser with ID user-123')
+	.required('create', 'Mode must be provided').describe('create', 'Create new user or wallet. Options: ["user", "wallet"]')
+	.describe('role', 'Define user role. Options: ["SR", "SP"]')
+	.describe('id', 'Define user ID')
+	.help('help')
+    .argv;
+
+
+const createUser = async () => {
+    const { id, role } = argv;
+    if (!id || !role || !['SR', 'SP'].includes(role)) {
+        console.log('Params are missing or wrong');
+        return;
+    }
+    await writeData({ id, role }, 'user', 'new');
+};
+
+const createWallet = async () => {
+    console.log('Creating wallet...');
+};
+
+if (argv.create === 'user') {
+    createUser();
+} else if (argv.create === 'wallet') {
+    createWallet();
+} else {
+    console.log('Wrong mode. Possible modes: ["user", "wallet"]');
+}
