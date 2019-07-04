ac2760a3c0de0647e8942df0d12bbc179266a19f
Alexey Sobolev <sobolev@mail.de>
2019-07-04 15:59:27 +0200

    Added new helpers

---
+++ ServiceApp/server/src/utils/tagHelper.ts
@@ -0,0 +1,6 @@
+import { zmq } from '../config.json';
+import { getCodeFromMessageType } from './eclassHelper';
+
+export const buildTag = (type, location = null) => {
+    return `${zmq.prefix}${getCodeFromMessageType(type)}${location || ''}`;
+};
