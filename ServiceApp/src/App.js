8fc261ef0d1fdf1a547b936931248e71e4c01817
Alexey Sobolev <sobolev@mail.de>
2019-07-05 19:32:19 +0200

    Added ZMQ listener for UI

---
+++ ServiceApp/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ServiceFactory } from './factories/serviceFactory';
+import { ApiClient } from './services/apiClient';
+import DashboardPage from './pages/dashboard';
+import config from './config.json';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this._configuration = {};
+
+        this.state = {
+            isBusy: true,
+            status: 'Loading Configuration...',
+            statusColor: 'info'
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            ServiceFactory.register('api', () => new ApiClient(config.apiEndpoint));
+
+            this._configuration = config;
+
+            this.setState({
+                isBusy: false,
+                status: '',
+                statusColor: 'success'
+            });
+        } catch (err) {
+            this.setState({
+                isBusy: false,
+                status: err.message,
+                statusColor: 'danger'
+            });
+        }
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                {!this.state.status && (
+                    <Switch>
+                        <Route path="/" component={DashboardPage} exact />
+                        <Route component={DashboardPage} />
+                    </Switch>
+                )}
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
