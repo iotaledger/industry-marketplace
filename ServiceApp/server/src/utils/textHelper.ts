13272ca7f7168353373ad6c7a920214defee2945
Alexey Sobolev <sobolev@mail.de>
2019-07-05 12:15:43 +0200

    Refactored helpers

--- ServiceApp/server/src/utils/textHelper.ts
+++ ServiceApp/server/src/utils/textHelper.ts
@@ -1,22 +1,17 @@
 /**
- * Helper functions for use with text.
+ * Encode Non ASCII characters to escaped characters.
+ * @param value The value to encode.
+ * @returns The encoded value.
  */
-export class TextHelper {
-    /**
-     * Encode Non ASCII characters to escaped characters.
-     * @param value The value to encode.
-     * @returns The encoded value.
-     */
-    public static encodeNonASCII(value) {
-        return value ? value.replace(/[\u007F-\uFFFF]/g, (chr) => `\\u${(`0000${chr.charCodeAt(0).toString(16)}`).substr(-4)}`) : undefined;
-    }
+export const encodeNonASCII = (value) => {
+    return value ? value.replace(/[\u007F-\uFFFF]/g, (chr) => `\\u${(`0000${chr.charCodeAt(0).toString(16)}`).substr(-4)}`) : undefined;
+};
 
-    /**
-     * Decode escaped Non ASCII characters.
-     * @param value The value to decode.
-     * @returns The decoded value.
-     */
-    public static decodeNonASCII(value) {
-        return value ? value.replace(/\\u([\d\w]{4})/gi, (match, grp) => String.fromCharCode(parseInt(grp, 16))) : undefined;
-    }
-}
+/**
+ * Decode escaped Non ASCII characters.
+ * @param value The value to decode.
+ * @returns The decoded value.
+ */
+export const decodeNonASCII = (value) => {
+    return value ? value.replace(/\\u([\d\w]{4})/gi, (match, grp) => String.fromCharCode(parseInt(grp, 16))) : undefined;
+};
