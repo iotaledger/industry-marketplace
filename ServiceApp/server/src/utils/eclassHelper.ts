4cd1bcb89dd920a42a4e8ef1610d922bbd3a70d6
fsommer95 <florian.sommer@st.ovgu.de>
2019-07-08 20:42:30 +0200

    pseudocode added

--- ServiceApp/server/src/utils/eclassHelper.ts
+++ ServiceApp/server/src/utils/eclassHelper.ts
@@ -1,27 +1,43 @@
 /**
- * Helper functions for eCl@ss IRDIs and lookup tables.
+ * Extract capital letter which follows the SEMARKET tag and specifies the message type
+ * @param tag The tag to process.
+ * @returns Found message type.
  */
-export class EClassHelper {
-    /**
-     * Extract capital letter which follows the SEMARKET tag and specifies the message type
-     * @param tag The tag to process.
-     * @returns Found message type.
-     */
-    public static extractMessageType(tag) {
-        const regex = /(?<=SEMARKET)([A-F])/gi;
-        const match = tag.match(regex);
-        const map = {
-            A: 'callForProposal',
-            B: 'proposal',
-            C: 'acceptProposal',
-            D: 'rejectProposal',
-            E: 'informConfirm',
-            F: 'informPayment'
-        };
+export const extractMessageType = (tag) => {
+    const regex = /(?<=SEMARKET)([A-F])/gi;
+    const match = tag.match(regex);
+    const map = {
+        A: 'callForProposal',
+        B: 'proposal',
+        C: 'acceptProposal',
+        D: 'rejectProposal',
+        E: 'informConfirm',
+        F: 'informPayment'
+    };
 
-        if (match !== null && match.length >= 1) {
-            return map[match[0]] || null;
-        }
-        return null;
+    if (match !== null && match.length >= 1) {
+        return map[match[0]] || null;
     }
-}
+    return null;
+};
+
+export const getCodeFromMessageType = message => {
+    const map = {
+        callForProposal: 'A',
+        proposal: 'B',
+        acceptProposal: 'C',
+        rejectProposal: 'D',
+        informConfirm: 'E',
+        informPayment: 'F'
+    };
+    return map[message] || null;
+};
+
+export const getRecieverID = message => {
+    return message.frame.sender.identification.id || null;
+};
+
+export const getcfpLocation = message => {
+    return message.dataElements._._.ort;
+};
+
