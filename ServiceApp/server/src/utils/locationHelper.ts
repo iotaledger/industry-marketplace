fe610a13b0a5a15fc60a4f33e46e94c25bb75e25
LisaSchneider94 <lisa.schneider@iota.org>
2019-07-17 14:41:20 +0200

    Add distance calculation

--- ServiceApp/server/src/utils/locationHelper.ts
+++ ServiceApp/server/src/utils/locationHelper.ts
@@ -1,3 +1,26 @@
 export const getLocationFromMessage = message => {
     return message.frame && message.frame.location ? message.frame.location : null;
 };
+
+
+export const calculateDistance = (lat1, lon1, lat2, lon2) => {
+
+    if ((lat1 === lat2) && (lon1 === lon2)) {
+        return 0;
+    }
+    else {
+        var R = 6371;
+        var dLat = (lat2 - lat1) * Math.PI / 180;
+        var dLon = (lon2 - lon1) * Math.PI / 180;
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        if (d > 1) {
+            return Math.round(d) + "km";
+        }
+        else if (d <= 1) { return Math.round(d * 1000) + "m"; }
+    }
+
+}
