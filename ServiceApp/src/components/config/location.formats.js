984f95d8e910b422c5e8f33f3927982db8f30fa1
Alexey Sobolev <sobolev@mail.de>
2019-07-25 14:23:44 +0200

    Refactoring, added configuration options

--- ServiceApp/src/components/config/location.formats.js
+++ ServiceApp/src/components/config/location.formats.js
@@ -4,21 +4,21 @@ import { addressApi } from '../../config.json';
 const locationFormats = [
   {
     name: 'Area Code',
-    action: async (sendMessage, areaCode) => await sendMessage({ areaCode })
+    action: async (sendMessage, areaCode, props) => await sendMessage({ areaCode, ...props })
   },
   {
     name: 'Address',
-    action: async (sendMessage, address) => {
+    action: async (sendMessage, address, props) => {
       // convert to area code
       const res = await axios.get(`${addressApi}?address=${encodeURI(address)}`);
       const areaCode = res.data;
-      return await sendMessage(areaCode ? { areaCode } : null);
+      return await sendMessage({ areaCode: areaCode || null, ...props });
     }
     },
   {
     name: 'GPS Coordinates',
-    action: async (sendMessage, gps) => {
-      return await sendMessage({ gps })
+    action: async (sendMessage, gps, props) => {
+      return await sendMessage({ gps, ...props })
     }
   }
 ]
