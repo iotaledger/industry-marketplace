cef327e66b58fb9fb5f8296f850deddf9eaaa426
Alexey Sobolev <sobolev@mail.de>
2019-07-25 20:34:07 +0200

    Minor fixes

--- ServiceApp/src/pages/details.js
+++ ServiceApp/src/pages/details.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import AssetNav from '../components/asset-nav';
 import Loading from '../components/loading';
