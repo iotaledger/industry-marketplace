5464803e97a477b897fefecd3b950ba6247daccd
Alexey Sobolev <sobolev@mail.de>
2019-07-25 20:28:18 +0200

    removed debug code

--- ServiceApp/src/pages/details.js
+++ ServiceApp/src/pages/details.js
@@ -15,7 +15,6 @@ class Details extends React.Component {
 
   async componentDidMount() {
     const { match: { params: { conversationId } } } = this.props;
-    console.log('componentDidMount', conversationId);
   }
 
 
