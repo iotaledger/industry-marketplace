1970132f548158ed507d1f33e175959502bab07a
Alexey Sobolev <sobolev@mail.de>
2019-07-26 15:14:18 +0200

    Added sensor data

--- ServiceApp/src/pages/details.js
+++ ServiceApp/src/pages/details.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
+import { stringify } from 'query-string';
 import AssetNav from '../components/asset-nav';
 import Loading from '../components/loading';
 import List from '../components/messages-list';
+import SensorData from '../components/sensor-data';
 import { fetch } from '../utils/mam';
 import api from '../utils/api';
+import { readFromStorage } from '../utils/storage';
+import { sensorDataDomain } from '../config.json';
 
 class Details extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: [],
+      sensorData: [],
       loading: false,
     };
   }
@@ -23,15 +29,31 @@ class Details extends React.Component {
         this.setState({ loading: true });
         fetch(data.root, data.side_key, this.appendToMessages, this.fetchComplete);
       }
+
+      const { sensorData: { deviceId, userId }} = await readFromStorage(conversationId);
+      const querystring = `${stringify({ deviceId, userId })}`;
+      const result = await axios.get(`${sensorDataDomain}${querystring}`);
+      const sensorDataSources = result.data;
+      if (sensorDataSources.length > 0) {
+        this.setState({ loading: true });
+        sensorDataSources.forEach(async ({ root, sidekey }) =>
+          await fetch(root, sidekey, this.appendToSensorData, this.fetchComplete)
+        );
+      };
     }
   }
 
   appendToMessages = message => this.setState({ messages: [...this.state.messages, message] });
+  appendToSensorData = data => {
+    const sensorData = [...this.state.sensorData, data]
+      .sort((a, b) => b.time - a.time);
+    this.setState({ sensorData })
+  };
 
   fetchComplete = () => this.setState({ loading: false });
 
   render() {
-    const { loading, messages } = this.state;
+    const { loading, messages, sensorData } = this.state;
 
     return (
       <Main>
@@ -43,9 +65,14 @@ class Details extends React.Component {
                 <Loading />
               </LoadingBox>
             ) : (
-              <Wrapper>
-                {messages.length > 0 ? <List messages={messages} /> : null}
-              </Wrapper>
+              <React.Fragment>
+                <Wrapper>
+                  {messages.length > 0 ? <List messages={messages} /> : null}
+                </Wrapper>
+                <Wrapper>
+                  {sensorData.length > 0 ? <SensorData sensorData={sensorData} /> : null}
+                </Wrapper>
+              </React.Fragment>
             )
           }
         </Data>
@@ -63,6 +90,7 @@ const Main = styled.main`
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   max-width: 1092px;
   width: 100%;
   margin: 20px auto 50px;
@@ -86,6 +114,8 @@ const Data = styled.section`
   min-height: 90vh;
   position: relative;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   @media (max-width: 760px) {
     flex-direction: column;
   }
