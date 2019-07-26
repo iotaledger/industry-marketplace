6565ccef2fdf75120222392a985a1e0433fd8d39
Alexey Sobolev <sobolev@mail.de>
2019-07-26 19:04:42 +0200

    CSS fixes

--- ServiceApp/src/components/sensor-data/index.js
+++ ServiceApp/src/components/sensor-data/index.js
@@ -18,10 +18,7 @@ const InfoCol = styled.main`
   position: relative;
   width: 880px;
   padding-left: 30px;
-  @media (max-width: 760px) {
-    width: 100%;
-    padding: 0;
-  }
+
   &::before {
     content: '';
     position: absolute;
@@ -30,11 +27,13 @@ const InfoCol = styled.main`
     width: 1px;
     height: 100%;
     background-color: #738fd4;
+
     @media (max-width: 1195px) {
-      left: 30px;
+      left: 245px;
     }
-    @media (max-width: 760px) {
-      visibility: hidden;
+
+    @media (max-width: 470px) {
+      left: 300px;
     }
   }
 `;
@@ -44,14 +43,11 @@ const CardWrapper = styled.div`
   flex-flow: row wrap;
   justify-content: space-between;
   padding: 40px 0 200px;
+
   @media (max-width: 1195px) {
     flex-flow: column nowrap;
     padding-bottom: 0;
     margin-left: 30px;
-  }
-  @media (max-width: 760px) {
-    width: 100%;
-    margin-left: 0;
     align-items: center;
   }
 `;
