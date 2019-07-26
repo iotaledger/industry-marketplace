87d73ef86401e98ed7fa78e6a338b33be10950e1
Alexey Sobolev <sobolev@mail.de>
2019-07-26 15:17:21 +0200

    Fixed assertion

--- ServiceApp/server/src/services/zmqService.ts
+++ ServiceApp/server/src/services/zmqService.ts
@@ -197,7 +197,7 @@ export class ZmqService {
         if (event === 'tx' && this._subscriptions[event]) {
             const messageType = extractMessageType(tag);
             
-            if (tag.startsWith(this._config.prefix) && messageType && operationList.includes(tag.slice(9, 15)) === true) {
+            if (tag.startsWith(this._config.prefix) && messageType && operationList.includes(tag.slice(9, 15))) {
                 const bundle = messageParams[8];
 
                 if (this.sentBundles.includes(bundle)) {
