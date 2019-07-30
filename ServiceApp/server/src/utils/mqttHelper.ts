a1b8c53fb7323c8fa4b54e194e1d06190d9cedbc
Lisa Schneider <50629860+LisaSchneider94@users.noreply.github.com>
2019-07-30 21:26:44 +0200

    Mqtt interface (#9)
    * add MQTT interface
    * fixed broker configuration
    * update Readme
    * fix appHelper
    * remove unnecessary changes
    * fixed PR

---
+++ ServiceApp/server/src/utils/mqttHelper.ts
@@ -0,0 +1,28 @@
+import mqtt from 'mqtt';
+import io from 'socket.io-client';
+import { mqttConfig } from '../config.json';
+
+
+const client = mqtt.connect(mqttConfig.broker)
+const socket = io(mqttConfig.domain);
+
+export const createHelperClient = () => {
+ 
+    return new Promise((resolve, reject) => {
+        try {
+            socket.emit('subscribe', { events: ['tx'] })
+            socket.on('subscribe', (data) => {
+                resolve(data.subscriptionIds[0]);
+            })
+        } catch (error) {
+            reject(error)
+        }
+    });
+}
+
+export const zmqToMQTT = topic =>
+    socket.on('zmq', data => client.publish(topic, JSON.stringify(data)));
+
+
+export const unsubscribeHelperClient = subscriptionId =>
+    socket.emit('unsubscribe', { subscriptionIds: [subscriptionId] });
