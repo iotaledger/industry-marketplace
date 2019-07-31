a2ffbde6b2a22de2f935b40912462816a34d63db
Alexey Sobolev <sobolev@mail.de>
2019-07-31 12:38:30 +0200

    Added missing mqtt lib

--- ServiceApp/server/src/utils/mqttHelper.ts
+++ ServiceApp/server/src/utils/mqttHelper.ts
@@ -2,27 +2,24 @@ import mqtt from 'mqtt';
 import io from 'socket.io-client';
 import { mqttConfig } from '../config.json';
 
-
-const client = mqtt.connect(mqttConfig.broker)
+const client = mqtt.connect(mqttConfig.broker);
 const socket = io(mqttConfig.domain);
 
 export const createHelperClient = () => {
- 
     return new Promise((resolve, reject) => {
         try {
-            socket.emit('subscribe', { events: ['tx'] })
-            socket.on('subscribe', (data) => {
+            socket.emit('subscribe', { events: ['tx'] });
+            socket.on('subscribe', data => {
                 resolve(data.subscriptionIds[0]);
-            })
+            });
         } catch (error) {
-            reject(error)
+            reject(error);
         }
     });
-}
+};
 
 export const zmqToMQTT = topic =>
     socket.on('zmq', data => client.publish(topic, JSON.stringify(data)));
-
 
 export const unsubscribeHelperClient = subscriptionId =>
     socket.emit('unsubscribe', { subscriptionIds: [subscriptionId] });
