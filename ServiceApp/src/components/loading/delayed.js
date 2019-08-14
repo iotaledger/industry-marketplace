964f8fcec7054a2abce9e15bd50112c6b223595b
Alexey Sobolev <sobolev@mail.de>
2019-08-14 10:54:48 +0200

    Fixed timeout

--- ServiceApp/src/components/loading/delayed.js
+++ ServiceApp/src/components/loading/delayed.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 class Delayed extends React.Component {
+    static timeout = null;
+
     constructor(props) {
         super(props);
         this.state = { hidden : true };
     }
 
     componentDidMount() {
-        setTimeout(() => {
+        this.timeout = setTimeout(() => {
             this.setState({ hidden: false });
         }, this.props.delay || 0);
+    }
+
+    componentWillUnmount() {
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
     }
 
     render() {
