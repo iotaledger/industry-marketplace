aca8ebbee50e41778b67f945d121b1479e1621a3
LisaSchneider94 <lisa.schneider@iota.org>
2019-08-15 18:16:48 +0200

    add balance check and repair fucntion for addresses

--- ServiceApp/server/src/utils/walletQueueHelper.ts
+++ ServiceApp/server/src/utils/walletQueueHelper.ts
@@ -1,8 +1,11 @@
-import { getRandomRow, updateValue } from './databaseHelper';
-
+import { getRandomRow, updateValue, readAllData, writeData } from './databaseHelper';
+import { getBalance } from './walletHelper';
+import { generateAddress } from '@iota/core';
 
 export const initializeWalletQueue = async () => {
-    
+
+    checkAddressBalance();
+
     interface IWallet {
         seed?: string;
     }
@@ -20,4 +23,28 @@ export const initializeWalletQueue = async () => {
     const newIncomingWallet: IWallet = await getRandomRow('wallet', 'status', 'usable');
     const reservedSeed = await newIncomingWallet.seed
     updateValue(reservedSeed, 'reserved')
+}
+
+const checkAddressBalance = async () => {
+
+    const wallet: any = await readAllData('wallet');
+
+    wallet.forEach(async ({ seed, address, keyIndex }) => {
+
+        //Check balance of all addresses 
+        let balance = await getBalance(address);
+        if (balance <= 0) {
+            [-2, -1, 0, 1, 2].forEach(async tempIndex => {
+                const index = Number(keyIndex) + Number(tempIndex)
+    
+                const newAddress = await generateAddress(seed, index);
+                balance = await getBalance(newAddress);
+
+                if (balance > 0) {
+                    await writeData('wallet', { address: newAddress, balance, keyIndex, seed });
+                }
+ 
+            })
+        }
+    });
 }
