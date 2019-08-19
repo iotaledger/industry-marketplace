bbb436af0a9ff9cd1466972b8873f3cb543ae9bb
Alexey Sobolev <sobolev@mail.de>
2019-08-19 15:13:14 +0200

    Allow accepting proposals only until contract starts

--- ServiceApp/src/components/card/asset.js
+++ ServiceApp/src/components/card/asset.js
@@ -139,6 +139,8 @@ const Asset = props => {
       setConfirmButtonEnabled(false);
     } else if (user.balance <= 0) {
       setConfirmButtonEnabled(false);
+    } if (user.role === 'SR' && asset.type === 'proposal' &&  Date.parse(asset.startTime) < Date.now()) {
+      setConfirmButtonEnabled(false);
     } else {
       setConfirmButtonEnabled(true);
       setPrice(asset.price);
