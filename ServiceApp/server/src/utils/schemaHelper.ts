cd7f313f320d3c0aef48961d071b368503f35baa
Yell0w <JelleFm@gmail.com>
2019-09-02 14:02:43 +0200

    Fixed Schema location

--- ServiceApp/server/src/utils/schemaHelper.ts
+++ ServiceApp/server/src/utils/schemaHelper.ts
@@ -1,5 +1,9 @@
-import * as fs from 'fs';
 import { SchemaManager } from 'identity_ts';
+const Schema1 = require('./../Schemas/0173-1#01-AAI711#001.json');
+const Schema2 = require('./../Schemas/0173-1#01-AAJ336#002.json');
+const Schema3 = require('./../Schemas/0173-1#01-AAO742#002.json');
+const Schema4 = require('./../Schemas/0173-1#01-AAP788#001.json');
+const Schema5 = require('./../Schemas/0173-1#01-BAF577#004.json');
 
 //TODO: Add DID's to trust
 export class SchemaHelper {
@@ -7,12 +11,12 @@ export class SchemaHelper {
 
     private constructor() {
         //Load all default application schemas
-        let folderPath : string = __dirname +"/../Schemas";
-        let filePaths : string[] = fs.readdirSync(folderPath);
-        for(let i=0; i < filePaths.length; i++) {
-            let fileName : string = filePaths[i].substr(0, filePaths[i].lastIndexOf('.'));
-            SchemaManager.GetInstance().AddSchemaFromFile(fileName, folderPath+"/"+filePaths[i]);
-        }
+        const manager = SchemaManager.GetInstance();
+        manager.AddSchema("0173-1#01-AAI711#001", Schema1);
+        manager.AddSchema("0173-1#01-AAJ336#002", Schema2);
+        manager.AddSchema("0173-1#01-AAO742#002", Schema3);
+        manager.AddSchema("0173-1#01-AAP788#001", Schema4);
+        manager.AddSchema("0173-1#01-BAF577#004", Schema5);
     }
 
     static GetInstance() : SchemaManager {
