0e68945449aad3b4f0320a7cc7bc1d59fbdb188c
Alexey Sobolev <sobolev@mail.de>
2019-09-04 14:48:46 +0200

    Styling

---
+++ SeMarketLanding/components/CallToAction.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import Text from './Text'
+import Button from './Button'
+
+import '../styles/content.scss'
+
+export default () => (
+    <div className="callToAction-wrapper">
+        <Text className="title">Explore for yourself</Text>
+        <Text>To see how the Semantic Marketplace operates, try our demo, or dive into the extended documentation to get started</Text>
+        <div className="callToAction-buttons-wrapper">
+            <Link prefetch href="/scalability">
+                <img className="intro-page secondary" src="../static/marketing_assets/landing/try_the_demo.svg"/>
+            </Link>
+            <Link prefetch href="/scalability">
+                <Button className="medium primary">
+                    Get started
+                </Button>
+            </Link>
+        </div>
+    </div>
+)
