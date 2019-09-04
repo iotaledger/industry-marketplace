72ae63792ed1782347410572f2335bbe8a390ed5
Alexey Sobolev <sobolev@mail.de>
2019-09-04 11:39:22 +0200

    Components

---
+++ SeMarketLanding/components/Logotypes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/content.scss'
+
+export default () => (
+    <div className="logotypes-wrapper">
+        <img src="../static/marketing_assets/logos/iota.svg" />
+        <img src="../static/marketing_assets/logos/eclass.svg" />
+        <img src="../static/marketing_assets/logos/ovgu.svg" />
+        <img src="../static/marketing_assets/logos/neoception.svg" />
+        <img src="../static/marketing_assets/logos/hsu.svg" />
+        <img src="../static/marketing_assets/logos/wewash.svg" />
+    </div>
+)
+
