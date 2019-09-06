351f8ff09b240057328daa0b3c196fdf73bccc13
Alexey Sobolev <sobolev@mail.de>
2019-09-06 17:13:20 +0200

    Fixed warnings

--- YellowPages/client/src/components/Carousel.js
+++ YellowPages/client/src/components/Carousel.js
@@ -3,21 +3,29 @@ import Slider from 'react-slick';
 import next from '../assets/img/video/arrow-right.svg';
 import prev from '../assets/img/video/arrow-left.svg';
 
-export default class extends React.Component {
-  render() {
+export default ({ items }) => {
+    const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+      <img {...props} src={prev} alt="" />
+    );
+
+    const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+      <img {...props} src={next} alt="" />
+    );
+
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
-      nextArrow: <img className="slider next" src={next} alt="" />,
-      prevArrow: <img className="slider prev" src={prev} alt="" />
+      prevArrow: <SlickArrowLeft />,
+      nextArrow: <SlickArrowRight />,
     };
+    
     return (
       <div className="slider-container">
         <Slider {...settings}>
-          {this.props.items.map((item, index) => (
+          {items.map((item, index) => (
             <div key={index} className="slider-content">
               {item}
             </div>
@@ -25,5 +33,4 @@ export default class extends React.Component {
         </Slider>
       </div>
     );
-  }
 }
