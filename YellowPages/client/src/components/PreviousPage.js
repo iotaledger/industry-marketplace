c63d031df7aefe954be148ecbd0b767ef1c3dc8b
Alexey Sobolev <sobolev@mail.de>
2019-09-15 13:48:40 +0200

    Animated buttons for chapters

--- YellowPages/client/src/components/PreviousPage.js
+++ YellowPages/client/src/components/PreviousPage.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Button from './Button'
 
-export default ({ page, title }) => (
+export default ({ page }) => (
     <Link to={`/${page}`}>
         <button className="navigation-btn back icon-arrow-left">
-            {title}
+            back
         </button>
     </Link>
 )
