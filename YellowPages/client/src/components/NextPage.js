0e11bf321a47652af316f7101b8e79d2e630fb62
Alexey Sobolev <sobolev@mail.de>
2019-09-15 14:49:50 +0200

    Page navigation language aware

--- YellowPages/client/src/components/NextPage.js
+++ YellowPages/client/src/components/NextPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import LanguageContext from '../context/language-context';
 
-export default ({ page }) => (
-    <Link to={`/${page}`}>
-        <button className="navigation-btn next icon-arrow-right">
-            next page
-        </button>
-    </Link>
-)
+export default ({ page }) => {
+  const { language } = useContext(LanguageContext);
+  const link = language === 'en' ? `/en/${page}` : `/de/${page}`;
+  console.log('Link', link);
+
+  return (
+      <Link to={link}>
+          <button className="navigation-btn next icon-arrow-right">
+              next page
+          </button>
+      </Link>
+  )
+}
--- YellowPages/client/src/components/PreviousPage.js
+++ YellowPages/client/src/components/PreviousPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import LanguageContext from '../context/language-context';
 
-export default ({ page }) => (
-    <Link to={`/${page}`}>
-        <button className="navigation-btn back icon-arrow-left">
-            back
-        </button>
-    </Link>
-)
+export default ({ page }) => {
+  const { language } = useContext(LanguageContext);
+  const link = language === 'en' ? `/en/${page}` : `/de/${page}`;
+  console.log('Link back', link, page);
+
+  return (
+      <Link to={page ? link : '/'}>
+          <button className="navigation-btn back icon-arrow-left">
+              back
+          </button>
+      </Link>
+  )
+}
