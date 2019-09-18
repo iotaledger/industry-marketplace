14bd4bafb30a342a1b442865e3fec97761828ec9
Alexey Sobolev <sobolev@mail.de>
2019-09-18 16:46:50 +0200

    Removed placeholder

--- YellowPages/client/src/components/WhatTheySay.js
+++ YellowPages/client/src/components/WhatTheySay.js
@@ -14,30 +14,30 @@ export default () => {
     const [showVideo, setShowVideo] = useState(false)
 
     const items = [
-        <React.Fragment>
-            <img
-                className="desktop"
-                role="button"
-                onClick={() => setShowVideo(true)}
-                src={videoDesktop}
-                alt=""
-            />
-            <img
-                className="mobile"
-                role="button"
-                onClick={() => setShowVideo(true)}
-                src={videoMobile}
-                alt=""
-            />
-            <div className="video-section-text-wrapper what-they-say">
-                <Text className="title">What they say</Text>
-                <Text className="name">Patrick Placeholder, IOTA Foundation</Text>
-                <Text className="quote">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</Text>
-                <Link to="/scalability">
-                    <Text className="link">See All Testimonioals</Text>
-                </Link>
-            </div>
-        </React.Fragment>,
+        // <React.Fragment>
+        //     <img
+        //         className="desktop"
+        //         role="button"
+        //         onClick={() => setShowVideo(true)}
+        //         src={videoDesktop}
+        //         alt=""
+        //     />
+        //     <img
+        //         className="mobile"
+        //         role="button"
+        //         onClick={() => setShowVideo(true)}
+        //         src={videoMobile}
+        //         alt=""
+        //     />
+        //     <div className="video-section-text-wrapper what-they-say">
+        //         <Text className="title">What they say</Text>
+        //         <Text className="name">Patrick Placeholder, IOTA Foundation</Text>
+        //         <Text className="quote">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</Text>
+        //         <Link to="/scalability">
+        //             <Text className="link">See All Testimonioals</Text>
+        //         </Link>
+        //     </div>
+        // </React.Fragment>,
         <React.Fragment>
             <img className="desktop face" src={person1} alt="Alexander Balyaev, Research Assistant, University of Magdeburg" />
             <div className="video-section-text-wrapper what-they-say">
