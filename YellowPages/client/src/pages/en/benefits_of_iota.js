7597d885897dab1714f3b0a0d8ca872b1bebe45e
Alexey Sobolev <sobolev@mail.de>
2019-09-18 16:49:31 +0200

    Adjusted video iframe

--- YellowPages/client/src/pages/en/benefits_of_iota.js
+++ YellowPages/client/src/pages/en/benefits_of_iota.js
@@ -23,16 +23,19 @@ export default () => {
                     <p>The manufacturing industry is undergoing a new revolution. Smart factories will share data to improve efficiency, increase production, reduce waste, and develop trust. But with the ever increasing connectivity between internal and external cyber-physical systems, security is of the utmost importance. One crucial component of security is data integrity.</p>
                     <p>At the same time, an I4.0 system needs a common infrastructure so that other systems and devices in the production or service chain can find, trust and interact with each other under basic security protocols the corresponding specifications are an ongoing development under Platform Industrie 4.0 Program).</p>
                     <p>IOTA’s underlying network, the Tangle, provides a trusted vendor-independent computing infrastructure for secure data and value transfer. The Tangle will serve as the backbone for I4.0, enabling I4.0 components to interact, trade directly with each other, and engage in the machine economy.</p>
+                </div>
                     <div className="video-container">
                         <iframe
+                            title="iota"
                             className="player"
                             type="text/html"
-                            width="100%"
-                            height="350px"
+                            width="65%"
+                            height="550px"
                             frameBorder="0"
                             src={videoSrc}
                         />
                     </div>
+                <div className="_markdown_">
                     <p>The Tangle forms the basis for the Industry Marketplace. It serves as a common communication and computing infrastructure able to support all actors involved in the value chain. It delivers trust and security by ensuring the reliable exchange of data and services provided by I4.0 components. A decentralized, immutable audit trail for asset offers, requests, and orders delivers increased trust among the transacting parties as well as full transparency and traceability for auditors.</p>
                     <p>The Tangle brings additional benefits:</p>
                     <ul>
