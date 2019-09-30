801ad4c2534ec8dd494f7330985bbd331dcfcc77
Alexey Sobolev <sobolev@mail.de>
2019-09-30 18:07:29 +0200

    Added link to video demonstration

--- YellowPages/client/src/pages/demo.js
+++ YellowPages/client/src/pages/demo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga';
 import styled from 'styled-components';
 import get from 'lodash-es/get';
+import ModalVideo from 'react-modal-video';
 import Map from '../components/map';
 import Layout from '../components/Layout';
 import AssetCards from '../components/asset-cards';
@@ -9,6 +10,7 @@ import AssetList from '../components/asset-list';
 import Zmq from '../components/zmq';
 import Text from '../components/Text';
 import Tabs from '../components/Tabs';
+import Button from '../components/Button';
 import { getAll, getByType, removeExpired, writeToStorage } from '../utils/storage';
 import { prepareData } from '../utils/card';
 import { serviceRequester, serviceProvider } from '../config.json';
@@ -27,7 +29,8 @@ export default class extends React.Component {
       assets: [],
       allAssets: [],
       activeSection: 'callForProposal',
-      view: 'grid'
+      view: 'grid',
+      showVideo: false
     };
 
     this.newMessage = this.newMessage.bind(this);
@@ -37,6 +40,7 @@ export default class extends React.Component {
   }
 
   async componentDidMount() {
+    window.scrollTo(0, 0);
     ReactGA.pageview('/demo');
     await this.checkExpired();
     this.timer = setInterval(() => this.checkExpired(), 600000);
@@ -56,18 +60,13 @@ export default class extends React.Component {
     await this.checkExpired();
   }
 
-  onScrollToTop() {
-    const target = document.querySelector('#main');
-    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-
   onTabChange(index) {
     const sections = ['callForProposal', 'proposal', 'acceptProposal'];
     this.setState({ activeSection: sections[index] }, async () => await this.checkExpired());
   }
 
   render() {
-    const { assets, allAssets, activeSection, view } = this.state;
+    const { assets, allAssets, activeSection, view, showVideo } = this.state;
     const externalService = activeSection === 'proposal' ? serviceProvider : serviceRequester;
     const statusNameMap = {
       callForProposal: 'Calls for Proposal',
@@ -90,22 +89,28 @@ export default class extends React.Component {
                   Try it as Service Provider
               </a>
             </h3>
+            <Button
+              className="medium secondary"
+              onClick={() => this.setState({ showVideo: true })}
+            >
+                Demonstration
+            </Button>
           </div>
           <div className="request-data-wrapper">
             <div className="demo-page-navigation">
               <div className="buttons-wrapper">
-                <Button
+                <ChangeViewButton
                   selected={view === 'grid'}
                   onClick={() => this.setState({ view: 'grid' })}
                 >
                   <img src={view === 'grid' ? grid_selected : grid} alt="Grid view" />
-                </Button>
-                <Button
+                </ChangeViewButton>
+                <ChangeViewButton
                   selected={view === 'list'}
                   onClick={() => this.setState({ view: 'list' })}
                 >
                   <img src={view === 'list' ? list_selected : list} alt="List view" />
-                </Button>
+                </ChangeViewButton>
               </div>
               <Tabs
                 view={view}
@@ -138,12 +143,20 @@ export default class extends React.Component {
           </div>
         </div>
         <Zmq callback={this.newMessage} />
+        <ModalVideo
+            channel='youtube'
+            autoplay
+            allowFullScreen
+            isOpen={showVideo}
+            videoId='471yyvWLfQk'
+            onClose={() => this.setState({ showVideo: false })}
+        />
       </Layout>
     );
   }
 }
 
-const Button = styled.button`
+const ChangeViewButton = styled.button`
   margin: 5px;
   background: transparent;
   border-radius: 6px;
