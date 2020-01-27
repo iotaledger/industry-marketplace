4b575df820b5a321ead6ed665ca8ff05c7a106b1
LisaSchneider94 <lisa.schneider@iota.org>
2020-01-27 14:38:40 +0100

    add setup for SR

--- ServiceApp/server/setupSP.sh
+++ ServiceApp/server/setupSP.sh
@@ -4,3 +4,4 @@
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP3  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create wallet &&
  node ./build/src/utils/userHelper.js --create wallet
+ node ./build/src/utils/userHelper.js --simulate --role SP 
