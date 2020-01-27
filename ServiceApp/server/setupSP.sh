f699ef74c58bbcf06277350fde77035bd268937e
LisaSchneider94 <lisa.schneider@iota.org>
2020-01-27 14:20:20 +0100

    add bin/bash

--- ServiceApp/server/setupSP.sh
+++ ServiceApp/server/setupSP.sh
@@ -1,5 +1,4 @@
-#!bash
- echo "Bashscript called"
+#!/bin/bash
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP1  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP2  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP3  --location 52.507339,13.377982 &&
