829cbf0eb86340eb61a7a986c16404170c89b653
LisaSchneider94 <lisa.schneider@iota.org>
2020-01-27 14:40:15 +0100

    fix bash

--- ServiceApp/server/setupSP.sh
+++ ServiceApp/server/setupSP.sh
@@ -3,5 +3,5 @@
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP2  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SP --name simSP3  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create wallet &&
- node ./build/src/utils/userHelper.js --create wallet
+ node ./build/src/utils/userHelper.js --create wallet &&
  node ./build/src/utils/userHelper.js --simulate --role SP 
