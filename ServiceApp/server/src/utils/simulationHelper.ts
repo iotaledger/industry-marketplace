5791ac7f8e57ae821a6301d8f657faf646dcb207
LisaSchneider94 <lisa.schneider@iota.org>
2020-02-04 16:31:51 +0100

    chang url

--- ServiceApp/server/src/utils/simulationHelper.ts
+++ ServiceApp/server/src/utils/simulationHelper.ts
@@ -154,7 +154,7 @@ const socket = io(`http://localhost:${port}/`);
 
         return new Promise(async (resolve, reject) => {
             try {
-                const response = await axios.post(`http://localhost:${argv.port}/${messageType}`, message, { timeout: 900000});
+                const response = await axios.post(`http://0.0.0.0:${argv.port}/${messageType}`, message, { timeout: 900000});
                 resolve(response.data);
             } catch (error) {
                 console.error('API Error', error);
