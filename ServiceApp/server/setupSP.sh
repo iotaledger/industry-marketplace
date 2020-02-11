3a98ab0e9616d06959593b432324d5cde3964f32
LisaSchneider94 <lisa.schneider@iota.org>
2020-02-11 10:45:01 +0100

    Switch to env port

--- ServiceApp/server/setupSP.sh
+++ ServiceApp/server/setupSP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- node ./build/src/utils/userHelper.js --create user --role SP --name simSP1  --location 52.507339,13.377982 &&
- node ./build/src/utils/userHelper.js --create user --role SP --name simSP2  --location 52.507339,13.377982 &&
- node ./build/src/utils/userHelper.js --create user --role SP --name simSP3  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SP --name SP1  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SP --name SP2  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SP --name SP3  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create wallet &&
- node ./build/src/utils/simulationHelper.js --simulate --role SP --port 3000
+ node ./build/src/utils/simulationHelper.js --simulate --role SP
--- ServiceApp/server/setupSR.sh
+++ ServiceApp/server/setupSR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- node ./build/src/utils/userHelper.js --create user --role SR --name simSR1  --location 52.507339,13.377982 &&
- node ./build/src/utils/userHelper.js --create user --role SR --name simSR2  --location 52.507339,13.377982 &&
- node ./build/src/utils/userHelper.js --create user --role SR --name simSR3  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SR --name SR1  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SR --name SR2  --location 52.507339,13.377982 &&
+ node ./build/src/utils/userHelper.js --create user --role SR --name SR3  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create wallet &&
- node ./build/src/utils/simulationHelper.js --simulate --role SR --port 5000
+ node ./build/src/utils/simulationHelper.js --simulate --role SR
