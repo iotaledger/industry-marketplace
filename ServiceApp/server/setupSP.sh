996f62fa4a48acd3237794ca3cd70df7fcaa9c7f
LisaSchneider94 <lisa.schneider@iota.org>
2020-02-18 15:19:29 +0100

    Remove debugging

--- ServiceApp/server/setupSP.sh
+++ ServiceApp/server/setupSP.sh
@@ -3,5 +3,5 @@
  node ./build/src/utils/userHelper.js --create user --role SP --name SP1  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SP --name SP2  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SP --name SP3  --location 52.507339,13.377982 &&
- #node ./build/src/utils/userHelper.js --create wallet &&
+ node ./build/src/utils/userHelper.js --create wallet &&
  node ./build/src/utils/simulationHelper.js --simulate --role SP
--- ServiceApp/server/setupSR.sh
+++ ServiceApp/server/setupSR.sh
@@ -3,5 +3,5 @@
  node ./build/src/utils/userHelper.js --create user --role SR --name SR1  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SR --name SR2  --location 52.507339,13.377982 &&
  node ./build/src/utils/userHelper.js --create user --role SR --name SR3  --location 52.507339,13.377982 &&
- #node ./build/src/utils/userHelper.js --create wallet &&
+ node ./build/src/utils/userHelper.js --create wallet &&
  node ./build/src/utils/simulationHelper.js --simulate --role SR
