d31412dcfdb67a4f7c922f81cf266d4e81e98946
LisaSchneider94 <lisa.schneider@iota.org>
2020-03-27 15:06:42 +0100

    fix

--- ServiceApp/server/src/utils/userHelper.ts
+++ ServiceApp/server/src/utils/userHelper.ts
@@ -36,7 +36,7 @@ const createNewWallet = async () => {
         if (newBalance > 0) {
             const faucet: IFaucet = { address: newAddress, balance: newBalance, keyIndex: index, seed: seed }
             const transfers = [{ address: wallet.address, value: 250000 }]
-            await transferFunds(faucet, 250000, transfers, true)
+            await transferFunds(faucet, 250000, transfers)
         }
     }
 
