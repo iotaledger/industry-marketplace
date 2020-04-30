6d21ddce5d95885d27a6066ea09b345aa8c58d99
LisaSchneider94 <lisa.schneider@iota.org>
2020-04-30 10:06:07 +0200

    Feature: Auto extract metric to text file

--- ServiceApp/server/src/utils/metricHelper.ts
+++ ServiceApp/server/src/utils/metricHelper.ts
@@ -1,9 +1,10 @@
 import io from 'socket.io-client';
 import get from 'lodash/get';
-import { readRow, updateValue, writeData } from '../utils/databaseHelper';
+import fs from 'fs';
+import { readRow, updateValue, writeData, removeData } from '../utils/databaseHelper';
 
 
-const socket = io('http://localhost:3000');
+const socket = io(`http://localhost:${process.env.PORT}`);
 
 socket.emit('subscribe', { events: ['tx'] })
 
@@ -18,6 +19,41 @@ socket.on('zmq', async (message) => {
     JSON.parse(data);
   }
 })
+
+const extractMetricToFile = async () => {
+  try {
+    
+    //Extract metric
+    const cfpCounter: any = await readRow('metric', 'context', 'cfp');
+    const proposalCounter: any = await readRow('metric', 'context', 'proposal');
+    const informPaymentCounter: any = await readRow('metric', 'context', 'informPayment');
+
+    const date = new Date().toLocaleString()
+    let payload = { date: date, cfpCounter, proposalCounter, informPaymentCounter }
+
+    //Append to file
+    fs.appendFile('metric.txt', JSON.stringify(payload, null, 2) + '\n' + '\n' + '\n' + '\n', function (err) {
+      if (err) throw err;
+      console.log(`Saved to metric on ${date}`);
+    });
+    
+    //Reset metric
+    await removeData('metric')
+    await writeData('metric', { "context": "cfp", "counter": 0 })
+    await writeData('metric', { "context": "proposal", "counter": 0 })
+    await writeData('metric', { "context": "informPayment", "counter": 0 })
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+setInterval(extractMetricToFile, Number(process.env.FREQUENCY))
+
+
+
+
 
 export const updateMetric = async (messageType) => {
 
