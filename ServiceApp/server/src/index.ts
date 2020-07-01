e5215369083e4f5ab95640fab13ff17d74642a27
Alexey Sobolev <sobolev@mail.de>
2020-07-01 14:15:33 +0200

    Added load balancer config

--- ServiceApp/server/src/index.ts
+++ ServiceApp/server/src/index.ts
@@ -1,5 +1,7 @@
+import { FailMode, LinearWalkStrategy, LoadBalancerSettings, SuccessMode } from '@iota/client-load-balancer';
 import { Server } from 'http';
 import SocketIO from 'socket.io';
+import { depth, minWeightMagnitude, providers } from './config.json';
 import { ServiceFactory } from './factories/serviceFactory';
 import { zmqSubscribe } from './routes/zmqSubscribe';
 import { zmqUnsubscribe } from './routes/zmqUnsubscribe';
@@ -10,6 +12,21 @@ AppHelper.build(
     (app, config, port) => {
         ServiceFactory.register('zmq', () => new ZmqService(config.zmq));
 
+        const loadBalancerSettings: LoadBalancerSettings = {
+            nodeWalkStrategy: new LinearWalkStrategy(
+                providers.map(provider => ({ provider }))
+            ),
+            depth,
+            mwm: minWeightMagnitude,
+            successMode: SuccessMode.keep,
+            failMode: FailMode.all,
+            timeoutMs: 10000,
+            failNodeCallback: (node, err) => {
+                console.log(`Failed node ${node.provider}, ${err.message}`);
+            }
+        };
+        ServiceFactory.register('load-balancer-settings', () => loadBalancerSettings);
+    
         const server = new Server(app);
         const socketServer = SocketIO(server);
 
