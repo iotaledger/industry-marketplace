d6b28c01fb554ae868ec1e336fe08baf731dcfc2
Alexey Sobolev <sobolev@mail.de>
2021-06-04 20:19:43 +0200

    clean up

--- ServiceApp/server/src/utils/helpers.ts
+++ ServiceApp/server/src/utils/helpers.ts
@@ -1,17 +1,3 @@
-import { trytesToAscii } from '@iota/converter';
-
-export const decodeMessage = transaction => {
-    // Modify to consumable length
-    if (!transaction.length || !transaction[0].signatureMessageFragment) {
-        return null;
-    }
-    const fragment = transaction[0].signatureMessageFragment;
-    const trytes = fragment % 2 !== 0 ? `${fragment}9` : fragment;
-
-    // Decode message
-    return trytesToAscii(trytes);
-};
-
 export const getNumberFromLetter = letter => {
     return letter.charCodeAt(0) - 65;
 };
